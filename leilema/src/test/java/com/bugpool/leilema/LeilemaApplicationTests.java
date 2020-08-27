0a56c1a3ec49bb3bd44a27001faaabe47e4ee02e
dgg_git <dgg@123>
2020-08-27 14:02:14 +0800

    create leilema repository

---
+++ leilema/src/test/java/com/bugpool/leilema/LeilemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugpool.leilema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeilemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
